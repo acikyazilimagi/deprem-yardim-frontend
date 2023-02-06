99f9833ca0963560ab1d4fe1f56bf02cef27a754
EmirBoyaci <emir@emirboyaci.dev>
2023-02-06 22:32:51 +0300

    ci: add github workflow for lint and code format check

--- components/UI/FooterBanner/FooterBanner.tsx
+++ components/UI/FooterBanner/FooterBanner.tsx
@@ -3,7 +3,11 @@ import styles from "./FooterBanner.module.css";
 export default function FooterBanner() {
   return (
     <span className={styles.footer}>
-     <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web siteleri gibi farklı kaynaklardan gelen tüm yardım çağrılarını topluyoruz ve bu veriyi sahada kullanılmak üzere anlamlı, rafine hale getiriyoruz. Amacımız bilgi teknolojilerini kullanarak ilgili kurum ve STK&apos;lara yardımcı olmak ve afet zamanlarında açık bir veri platformu sağlamak.
+      <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web siteleri gibi
+      farklı kaynaklardan gelen tüm yardım çağrılarını topluyoruz ve bu veriyi
+      sahada kullanılmak üzere anlamlı, rafine hale getiriyoruz. Amacımız bilgi
+      teknolojilerini kullanarak ilgili kurum ve STK&apos;lara yardımcı olmak ve
+      afet zamanlarında açık bir veri platformu sağlamak.
     </span>
   );
 }
