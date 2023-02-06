84de9a8f339b641e7007b41bbce41de91fb7a18e
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2023-02-06 14:38:34 +0100

    feat(#23) Harita merkez koordinatlarini degistirir
    Harita koordinatlarini depremden etkilenen bolgeye odaklar.

--- components/Map.tsx
+++ components/Map.tsx
@@ -7,8 +7,8 @@ const Map = () => {
   console.log(styles.leafletMap);
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={[37.2544, 37.3315]}
+      zoom={9}
       scrollWheelZoom={false}
       className={styles.leafletMap}
     >
