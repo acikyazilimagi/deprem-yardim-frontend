79a5e3c6b053c0ace705af0a576f9a8b3f656cbc
Mehmet Sait isik <mehmet.isik@morhipo.com>
2023-02-07 01:18:49 +0300

    footer alanı için göster gizle butonu eklenmesi

--- components/UI/FooterBanner/FooterBanner.tsx
+++ components/UI/FooterBanner/FooterBanner.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 import styles from "./FooterBanner.module.css";
 
-export default function FooterBanner() {
+const FooterBanner = () => {
+  const [show, setShow] = useState<boolean>(false);
+
   return (
-    <span className={styles.footer}>
-      <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web siteleri gibi
-      farklı kaynaklardan gelen tüm yardım çağrılarını topluyoruz ve bu veriyi
-      sahada kullanılmak üzere anlamlı, rafine hale getiriyoruz. Amacımız bilgi
-      teknolojilerini kullanarak ilgili kurum ve STK&apos;lara yardımcı olmak ve
-      afet zamanlarında açık bir veri platformu sağlamak.
-    </span>
+    <div className={styles.footer}>
+      <>
+        <span className={styles.showButton} onClick={() => setShow(!show)}>
+          {show === false ? "Açıklama Göster" : "Gizle"}
+        </span>
+      </>
+
+      {show === true ? (
+        <div className={styles.bottom}>
+          <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web siteleri
+          gibi farklı kaynaklardan gelen tüm yardım çağrılarını topluyoruz ve bu
+          veriyi sahada kullanılmak üzere anlamlı, rafine hale getiriyoruz.
+          Amacımız bilgi teknolojilerini kullanarak ilgili kurum ve
+          STK&apos;lara yardımcı olmak ve afet zamanlarında açık bir veri
+          platformu sağlamak.
+        </div>
+      ) : null}
+    </div>
   );
-}
+};
+
+export default FooterBanner;
