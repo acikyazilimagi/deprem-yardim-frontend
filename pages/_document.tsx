5768f7c9913d7b574783f6c8844340360811fe50
Yasin Elbüz <yasinelbuz@gmail.com>
2023-02-06 23:04:14 +0300

    feat(logo): add logo and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="logo.svg" />
           <meta name="emotion-insertion-point" content="" />
           {(this.props as any).emotionStyleTags}
         </Head>
