b1a8e6d2174902398073975d71b13aad984280ac
maliksenpai <cramixzx@hotmail.com>
2023-02-06 21:06:18 +0300

    feat: add google map for drawer and add textfield for link

--- components/UI/Drawer/Drawer.tsx
+++ components/UI/Drawer/Drawer.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from "react";
+import React, { useMemo, useState } from "react";
 import Box from "@mui/material/Box";
 import { default as MuiDrawer } from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -7,9 +7,9 @@ import Tag from "../Tag/Tag";
 import { Tags } from "../Tag/Tag.types";
 import CloseIcon from "@mui/icons-material/Close";
 import { useWindowSize } from "@/hooks/useWindowsSize";
-import {IconButton, InputAdornment, Snackbar, TextField} from "@mui/material";
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import {IMarker} from "@/components/UI/Map/utils";
+import { IconButton, InputAdornment, Snackbar, TextField } from "@mui/material";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import { IMarker } from "@/components/UI/Map/utils";
 
 export default function Drawer({
   isOpen,
@@ -24,7 +24,7 @@ export default function Drawer({
   const [openBillboardSnackbar, setOpenBillboardSnackbar] = useState(false);
 
   function openGoogleMap(lat: string, lng: string) {
-    window.open(`https://www.google.com/maps/@${lat},${lng},14z`, '_blank');
+    window.open(`https://www.google.com/maps/@${lat},${lng},14z`, "_blank");
   }
 
   function copyBillboard(url: string) {
@@ -49,16 +49,30 @@ export default function Drawer({
           <Tag color={Tags[intensity]?.color}>{Tags[intensity]?.intensity}</Tag>
           <h3>{name}</h3>
           <p> {`${lat}"N ${lng}"E`}</p>
-          <Button onClick={() => openGoogleMap(lat.toString(), lng.toString())}> Google Map&apos;te Göster</Button>
+          <Button onClick={() => openGoogleMap(lat.toString(), lng.toString())}>
+            {" "}
+            Google Map&apos;te Göster
+          </Button>
           <div>
-            <TextField value={`https://www.google.com/maps/@${data?.lat},${data?.lng},14z`} InputProps={{
-                endAdornment: <InputAdornment position="start">
-                    <IconButton onClick={() => copyBillboard(`https://www.google.com/maps/@${data?.lat},${data?.lng},14z`)}>
-                        <FileCopyIcon />
+            <TextField
+              value={`https://www.google.com/maps/@${data?.lat},${data?.lng},14z`}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton
+                      onClick={() =>
+                        copyBillboard(
+                          `https://www.google.com/maps/@${data?.lat},${data?.lng},14z`
+                        )
+                      }
+                    >
+                      <FileCopyIcon />
                     </IconButton>
-                </InputAdornment>,
+                  </InputAdornment>
+                ),
                 readOnly: true,
-            }} />
+              }}
+            />
           </div>
         </div>
         <CloseIcon className={styles.closeButton} />
@@ -69,10 +83,10 @@ export default function Drawer({
   return (
     <div>
       <Snackbar
-          open={openBillboardSnackbar}
-          autoHideDuration={2000}
-          onClose={() => setOpenBillboardSnackbar(false)}
-          message="Adres Kopyalandı"
+        open={openBillboardSnackbar}
+        autoHideDuration={2000}
+        onClose={() => setOpenBillboardSnackbar(false)}
+        message="Adres Kopyalandı"
       />
       <Button onClick={(e: any) => toggler(e)}>Left</Button>
       <MuiDrawer
