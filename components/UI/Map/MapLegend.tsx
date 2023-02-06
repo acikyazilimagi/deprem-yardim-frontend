0b365f7d88d521a21f7334db9a9540632b1d42c0
ncu <celierea@gmail.com>
2023-02-07 00:58:44 +0300

    Toggleable legend on big screens

--- components/UI/Map/MapLegend.tsx
+++ components/UI/Map/MapLegend.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./Map.module.css";
 import { Tags } from "../Tag/Tag.types";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import CloseIcon from "@mui/icons-material/Close";
 
 function MapLegend() {
+  const [isLegendOpen, setIsLegendOpen] = useState(false);
+
+  function toggleLegend() {
+    setIsLegendOpen(!isLegendOpen);
+  }
+
   return (
     <>
-      <div className={styles.legend}>
+      <div
+        className={`${styles.legend}${isLegendOpen ? " " + styles.open : ""}`}
+      >
+        {!isLegendOpen && (
+          <ArrowBackIosIcon color="action" onClick={toggleLegend} />
+        )}
+        {isLegendOpen && <CloseIcon color="action" onClick={toggleLegend} />}
         {Object.keys(Tags).map((intensity, index) => (
           <div className={styles.legend_item} key={index}>
             <div
