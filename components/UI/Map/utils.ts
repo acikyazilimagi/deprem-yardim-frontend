937a9c9cb6862b07dd02ae2357718f07894d626e
Mehmet Yağız Maktav <115627863+myagizmaktav@users.noreply.github.com>
2023-02-06 22:47:03 +0300

    feat: adding heatmap to marker

--- components/UI/Map/utils.ts
+++ components/UI/Map/utils.ts
@@ -1,5 +1,6 @@
 export const DEFAULT_CENTER = [37.0588348, 37.3450317];
 export const DEFAULT_ZOOM = 13;
+export const DEFAULT_IMPORTANCY = 1;
 
 export const markers = [
   {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,22 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 
 import Container from "@mui/material/Container";
-import LeafletMap from "@/components/UI/Map";
+// import LeafletMap from "@/components/UI/Map";
 import { useState, useCallback } from "react";
 import Drawer from "@/components/UI/Drawer/Drawer";
 import FooterBanner from "@/components/UI/FooterBanner/FooterBanner";
 import { IMarker } from "@/components/UI/Map/utils";
 import { CleaningServices } from "@mui/icons-material";
 import { Data, MarkerData } from "../mocks/types";
+import dynamic from "next/dynamic";
 
 export default function Home({ results }: { results: MarkerData[] }) {
   const [isOpen, setisOpen] = useState(false);
   const [drawerData, setDrawerData] = useState<IMarker>();
 
+  const LeafletMap = dynamic(() => import("@/components/UI/Map"), {
+    ssr: false,
+  });
   const toggleDrawer = useCallback(
     () => (event: React.KeyboardEvent | React.MouseEvent, markerData?: any) => {
       if (
