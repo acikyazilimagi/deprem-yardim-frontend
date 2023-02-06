ba3e9179b4946a60785350d6c374555735ec925b
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-02-06 17:50:42 +0300

    fix: map icons

---
+++ components/UI/Map/DynamicMap.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import Leaflet from "leaflet";
+import * as ReactLeaflet from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import styles from "./Map.module.css";
+
+const { MapContainer } = ReactLeaflet;
+
+const Map = ({ children, className, ...rest }: any) => {
+  let mapClassName = styles.map;
+
+  if (className) {
+    mapClassName = `${mapClassName} ${className}`;
+  }
+
+  useEffect(() => {
+    (async function init() {
+      // @ts-ignore
+      delete Leaflet.Icon.Default.prototype._getIconUrl;
+      Leaflet.Icon.Default.mergeOptions({
+        iconRetinaUrl: "leaflet/images/marker-icon-2x.png",
+        iconUrl: "leaflet/images/marker-icon.png",
+        shadowUrl: "leaflet/images/marker-shadow.png",
+      });
+    })();
+  }, []);
+
+  return (
+    <MapContainer className={mapClassName} {...rest}>
+      {children(ReactLeaflet, Leaflet)}
+    </MapContainer>
+  );
+};
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "node_modules/leaflet/dist/images",
+            to: path.resolve(__dirname, "public", "leaflet", "images"),
+          },
+        ],
+      })
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
