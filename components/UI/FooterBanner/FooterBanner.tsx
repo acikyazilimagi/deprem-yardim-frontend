89bf711a305f9b39bde4c590ea32fbc5dcebd6ed
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-02-06 18:34:21 +0100

    feat: footer banner style changed

--- components/UI/FooterBanner/FooterBanner.tsx
+++ components/UI/FooterBanner/FooterBanner.tsx
@@ -2,10 +2,8 @@ import styles from "./FooterBanner.module.css";
 
 export default function FooterBanner() {
   return (
-    <div className={styles.wrapper}>
-      <span className={styles.text}>
-        <b>Duyuru:</b> 6 Şubat 2023 depreminde sosyal medyada paylaşılan deprem ile ilgili paylaşımları anlamlandırarak yardım kuruluşlarına destek verilmesi amaçlanmıştır.
-      </span>
-    </div>
+    <span className={styles.footer}>
+      <b>Duyuru:</b> 6 Şubat 2023 depreminde sosyal medyada paylaşılan deprem ile ilgili paylaşımları anlamlandırarak yardım kuruluşlarına destek verilmesi amaçlanmıştır.
+    </span>
   )
 }
