2f364ad7648da98fe07ff33bfa264959ec1b001e
ncu <celierea@gmail.com>
2023-02-06 23:05:11 +0300

    Fetch intensity tags from tag type decleration file.

--- components/UI/Map/MapLegend.tsx
+++ components/UI/Map/MapLegend.tsx
@@ -1,44 +1,20 @@
 import React from "react";
 import styles from "./Map.module.css";
-
-const LEGEND_TYPES = [
-  {
-    text: "güvenli alanlar",
-    color: "#0288D1",
-  },
-  {
-    text: "düşük yoğunluk",
-    color: "#CFCECD",
-  },
-  {
-    text: "orta-alt yoğunluk",
-    color: "#FAF7BF",
-  },
-  {
-    text: "orta yoğunluk",
-    color: "#EE181B",
-  },
-  {
-    text: "orta-üst yoğunluk",
-    color: "#F0944B",
-  },
-  {
-    text: "yoğun",
-    color: "#74080A",
-  },
-];
+import { Tags } from "../Tag/Tag.types";
 
 function MapLegend() {
   return (
     <>
       <div className={styles.legend}>
-        {LEGEND_TYPES.map((item, index) => (
+        {Object.keys(Tags).map((intensity, index) => (
           <div className={styles.legend_item} key={index}>
             <div
               className={styles.legend_item__color}
-              style={{ backgroundColor: item.color }}
+              style={{ backgroundColor: Tags[intensity].color }}
             ></div>
-            <div className={styles.legend_item__text}>{item.text}</div>
+            <div className={styles.legend_item__text}>
+              {Tags[intensity].intensity}
+            </div>
           </div>
         ))}
       </div>
