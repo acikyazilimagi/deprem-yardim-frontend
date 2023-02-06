403e8688d8b64f49fdafc55912d7a25791ceb2bc
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-02-06 16:19:49 +0300

    fix: map height

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import styles from "./Map.module.css";
 
 const Map = () => {
+  console.log(styles.leafletMap);
   return (
-    <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className={styles.leafletMap}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={[51.505, -0.09]}>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Marker>
+    </MapContainer>
   );
 };
 
