bfe1428be5f952ce83601fcba04c43c6767905be
Ahmet Çağın Sucu <caginsucuu@gmail.com>
2023-02-07 01:32:54 +0300

    feat: update legend with popover

--- components/UI/Map/MapLegend.tsx
+++ components/UI/Map/MapLegend.tsx
@@ -3,6 +3,7 @@ import styles from "./Map.module.css";
 import { Tags } from "../Tag/Tag.types";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import CloseIcon from "@mui/icons-material/Close";
+import { Popover, Typography } from "@mui/material";
 
 function MapLegend() {
   const [isLegendOpen, setIsLegendOpen] = useState(false);
@@ -26,17 +27,56 @@ function MapLegend() {
           <ArrowBackIosIcon color="action" onClick={toggleLegend} />
         )}
         {Object.keys(Tags).map((intensity, index) => (
-          <div className={styles.legend_item} key={index}>
-            <div
-              className={styles.legend_item__color}
-              style={{ backgroundColor: Tags[intensity].color }}
-            ></div>
-            <div className={styles.legend_item__text}>
-              {Tags[intensity].intensity}
-            </div>
+          <div key={index}>
+            <PopOver
+              title={Tags[intensity].intensity}
+              color={Tags[intensity].color}
+            />
           </div>
         ))}
       </div>
+    </>
+  );
+}
+
+function PopOver({ title, color }: { title: string; color: string }) {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+  return (
+    <>
+      <button
+        className={styles.legend_item}
+        aria-describedby={id}
+        onClick={handleClick}
+      >
+        <div
+          className={styles.legend_item__color}
+          style={{ backgroundColor: color }}
+        ></div>
+        <span className={styles.legend_item__text}>{title}</span>
+      </button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <Typography sx={{ p: 2 }}>{title}</Typography>
+      </Popover>
     </>
   );
 }
