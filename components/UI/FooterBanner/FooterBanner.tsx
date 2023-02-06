3e517d58b59425ae162fa6853334013f297d6787
EmirBoyaci <emir@emirboyaci.dev>
2023-02-07 01:24:18 +0300

    feat: add render if component to make conditional rendering easily

--- components/UI/FooterBanner/FooterBanner.tsx
+++ components/UI/FooterBanner/FooterBanner.tsx
@@ -1,3 +1,4 @@
+import RenderIf from "@/components/UI/Common/RenderIf";
 import { useState } from "react";
 import styles from "./FooterBanner.module.css";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
@@ -5,20 +6,21 @@ import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 export default function FooterBanner() {
   const [hideFooter, setHideFooter] = useState(false);
 
-  if (hideFooter) return <></>;
-
   return (
-    <span className={styles.footer}>
-      <span>
-        <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web siteleri
-        gibi farklı kaynaklardan gelen tüm yardım çağrılarını topluyoruz ve bu
-        veriyi sahada kullanılmak üzere anlamlı, rafine hale getiriyoruz.
-        Amacımız bilgi teknolojilerini kullanarak ilgili kurum ve STK&apos;lara
-        yardımcı olmak ve afet zamanlarında açık bir veri platformu sağlamak.
+    <RenderIf condition={!hideFooter}>
+      <span className={styles.footer}>
+        <span>
+          <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web siteleri
+          gibi farklı kaynaklardan gelen tüm yardım çağrılarını topluyoruz ve bu
+          veriyi sahada kullanılmak üzere anlamlı, rafine hale getiriyoruz.
+          Amacımız bilgi teknolojilerini kullanarak ilgili kurum ve
+          STK&apos;lara yardımcı olmak ve afet zamanlarında açık bir veri
+          platformu sağlamak.
+        </span>
+        <span>
+          <HighlightOffIcon onClick={() => setHideFooter(true)} />
+        </span>
       </span>
-      <span>
-        <HighlightOffIcon onClick={() => setHideFooter(true)} />
-      </span>
-    </span>
+    </RenderIf>
   );
 }
