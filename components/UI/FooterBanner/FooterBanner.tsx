cc3b89874d1dfb3450d88bc37224a0f97d81bc27
Ahmet Eren Odacı <33299067+ahmeterenodaci@users.noreply.github.com>
2023-02-07 11:59:08 +0300

    fix( #163): uyarı kapatılınca yardım aşağı kaysın

--- components/UI/FooterBanner/FooterBanner.tsx
+++ components/UI/FooterBanner/FooterBanner.tsx
@@ -2,25 +2,29 @@ import RenderIf from "@/components/UI/Common/RenderIf";
 import { useState } from "react";
 import styles from "./FooterBanner.module.css";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import { HelpButton } from "@/components/UI/Button/HelpButton";
 
 export default function FooterBanner() {
   const [hideFooter, setHideFooter] = useState(false);
 
   return (
-    <RenderIf condition={!hideFooter}>
-      <span className={styles.footer}>
-        <span>
-          <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web siteleri
-          gibi farklı kaynaklardan gelen tüm yardım çağrılarını topluyoruz ve bu
-          veriyi sahada kullanılmak üzere anlamlı, rafine hale getiriyoruz.
-          Amacımız bilgi teknolojilerini kullanarak ilgili kurum ve
-          STK&apos;lara yardımcı olmak ve afet zamanlarında açık bir veri
-          platformu sağlamak.
+    <footer className={styles.footer}>
+      <HelpButton />
+      <RenderIf condition={!hideFooter}>
+        <span className={styles.dismissible}>
+          <span>
+            <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web
+            siteleri gibi farklı kaynaklardan gelen tüm yardım çağrılarını
+            topluyoruz ve bu veriyi sahada kullanılmak üzere anlamlı, rafine
+            hale getiriyoruz. Amacımız bilgi teknolojilerini kullanarak ilgili
+            kurum ve STK&apos;lara yardımcı olmak ve afet zamanlarında açık bir
+            veri platformu sağlamak.
+          </span>
+          <span>
+            <HighlightOffIcon onClick={() => setHideFooter(true)} />
+          </span>
         </span>
-        <span>
-          <HighlightOffIcon onClick={() => setHideFooter(true)} />
-        </span>
-      </span>
-    </RenderIf>
+      </RenderIf>
+    </footer>
   );
 }
