6cbc3d514d082d3e61cb3c189e11b899751550d4
onurkanbakirci <onurkan_bkrc@hotmail.com>
2023-02-07 15:35:04 +0300

    android app is initialized

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.depremyardim.app",
+  appName: "Deprem Yardim",
+  webDir: "out",
+  bundledWebRuntime: false,
+};
+
+export default config;
