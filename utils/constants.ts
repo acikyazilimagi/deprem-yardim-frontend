84371c25c66c937968006b8d3cb6e70d365d56a4
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-02-07 18:53:19 +0300

    fix: ilk renderda hatay gösterme problemi

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  "https://api.afetharita.com/tweets/areas?ne_lat=36.2354052&ne_lng=36.169436&sw_lat=36.2354052&sw_lng=36.169436";
+  "https://api.afetharita.com/tweets/areas?ne_lat=100&ne_lng=0&sw_lat=100&sw_lng=0";
