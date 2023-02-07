381c1456fc699f04487998242c8b64c91c3c7226
Eray Gündoğmuş <74984741+eraygundogmus@users.noreply.github.com>
2023-02-07 09:55:18 +0300

    Mucahitgurbuz set map bounds (#154)
    * feat: set map max bounds and min zoom
    
    * fix: partytown and sw.js.map added to git-ignore
    
    * style: add logo style
    
    * feat: logo css updated
    
    * fix: map legend css radius fixed
    
    * chore: Type `LocationsResponse`
    
    * feat: Use real backend URL and transform response
    
    * feat: Make data fetching non-blocking
    
    * chore: merge and fix conflict
    
    * feat: Debounce fetch url
    
    * feat: set border-rad + align
    
    * Revert "New backend API integration"
    
    * chore: Type `LocationsResponse`
    
    * feat: Use real backend URL and transform response
    
    * feat: Make data fetching non-blocking
    
    * feat: Get locations by exact coordinates according to visible map area
    
    * feat: Debounce fetch url
    
    * chore: Make NEXT_PUBLIC_API_MOCKING false
    
    * chore: ignore react-hooks/exhaustive-deps for the useEffect where we fetch the data
    
    * feat: add impacted city button to the right side
    
    * feat: add impacted city button to the right side
    
    * Revert "feat: add impacted city button to the right side"
    
    * chore: comment out city buttons
    
    * Puskuruk new backend integration (#151)
    
    * fix: fixed debounce issue
    
    * fix: reduce debounce timer
    
    * fix: reduce debounce timer
    
    * refactor: removed unnecessary return statement
    
    ---------
    
    Co-authored-by: Mücahit Gürbüz <mucahitgurbuz@gmail.com>
    Co-authored-by: Serhan Elmalı <serhanelmali@gmail.com>
    Co-authored-by: Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
    Co-authored-by: Tamay Eser Uysal <tamayeseruysal@gmail.com>
    Co-authored-by: Kerem Nalbant <kerem.nalbant@hotmail.com>
    Co-authored-by: Ekin Dursun <ekindursun@gmail.com>
    Co-authored-by: Mert Kaya <47050442+mertkaya1033@users.noreply.github.com>

--- components/UI/Map/utils.ts
+++ components/UI/Map/utils.ts
@@ -1,3 +1,10 @@
-export const DEFAULT_CENTER = [37.0588348, 37.3450317];
+import { LatLngBoundsExpression, LatLngExpression } from "leaflet";
+
+export const DEFAULT_CENTER: LatLngExpression = [37.0588348, 37.3450317];
 export const DEFAULT_ZOOM = 13;
 export const DEFAULT_IMPORTANCY = 1;
+export const DEFAULT_MAX_BOUNDS: LatLngBoundsExpression = [
+  [39.684006, 31.607263],
+  [35.459834, 45.281141],
+];
+export const DEFAULT_MIN_ZOOM = 8;
