7d57749639c646261e4b8d931dff63e578cd8d3b
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-02-07 21:51:06 +0300

    fix: change default center to new coordinates
    new coordinates: pazarcik/kahramanmaraş

--- components/UI/Map/utils.ts
+++ components/UI/Map/utils.ts
@@ -1,6 +1,6 @@
 import { LatLngBoundsExpression, LatLngExpression } from "leaflet";
 
-export const DEFAULT_CENTER: LatLngExpression = [37.0588348, 37.3450317];
+export const DEFAULT_CENTER: LatLngExpression = [37.4857521, 37.2890447];
 export const DEFAULT_ZOOM = 6.4;
 export const DEFAULT_ZOOM_MOBILE = 7.3;
 export const DEFAULT_IMPORTANCY = 1;
