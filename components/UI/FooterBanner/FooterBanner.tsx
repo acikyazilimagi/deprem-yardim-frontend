755ba6659a15695d6ea8e148daf8e485b9145202
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-02-07 12:51:36 +0100

    fix: footer logo css overlap issue fixed

--- components/UI/FooterBanner/FooterBanner.tsx
+++ components/UI/FooterBanner/FooterBanner.tsx
@@ -9,22 +9,24 @@ export default function FooterBanner() {
 
   return (
     <footer className={styles.footer}>
-      <HelpButton />
-      <RenderIf condition={!hideFooter}>
-        <span className={styles.dismissible}>
-          <span>
-            <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web
-            siteleri gibi farklı kaynaklardan gelen tüm yardım çağrılarını
-            topluyoruz ve bu veriyi sahada kullanılmak üzere anlamlı, rafine
-            hale getiriyoruz. Amacımız bilgi teknolojilerini kullanarak ilgili
-            kurum ve STK&apos;lara yardımcı olmak ve afet zamanlarında açık bir
-            veri platformu sağlamak.
+      <div className={styles.footerWrapper}>
+        <HelpButton />
+        <RenderIf condition={!hideFooter}>
+          <span className={styles.dismissible}>
+            <span>
+              <b>Açıklama:</b> Twitter, Instagram, Whatsapp ve çeşitli web
+              siteleri gibi farklı kaynaklardan gelen tüm yardım çağrılarını
+              topluyoruz ve bu veriyi sahada kullanılmak üzere anlamlı, rafine
+              hale getiriyoruz. Amacımız bilgi teknolojilerini kullanarak ilgili
+              kurum ve STK&apos;lara yardımcı olmak ve afet zamanlarında açık
+              bir veri platformu sağlamak.
+            </span>
+            <span>
+              <HighlightOffIcon onClick={() => setHideFooter(true)} />
+            </span>
           </span>
-          <span>
-            <HighlightOffIcon onClick={() => setHideFooter(true)} />
-          </span>
-        </span>
-      </RenderIf>
+        </RenderIf>
+      </div>
     </footer>
   );
 }
