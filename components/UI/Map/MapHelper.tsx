e49be000df87d4e6088cb85f35b152255a2dd475
maliksenpai <cramixzx@hotmail.com>
2023-02-08 01:00:04 +0300

    feat: add map helper for user

--- components/UI/Map/MapHelper.tsx
+++ components/UI/Map/MapHelper.tsx
@@ -1,7 +1,10 @@
-import { Box, Button, Popover } from "@mui/material";
+import { Box, Button, Chip, Popover } from "@mui/material";
 import React, { useState } from "react";
 import { Help } from "@mui/icons-material";
 import { useDevice } from "@/stores/mapStore";
+import "./Map.module.css";
+import { Tags } from "@/components/UI/Tag/Tag.types";
+import styles from "@/components/UI/Map/Map.module.css";
 
 const HELPER_ID = "map-helper";
 
@@ -33,6 +36,10 @@ function MapHelper() {
           minWidth: "30px",
           width: "30px",
           height: "30px",
+          backgroundColor: "#f54a4a",
+          ":hover": {
+            backgroundColor: "#f54a4a",
+          },
         }}
         aria-describedby={HELPER_ID}
         variant={"contained"}
@@ -56,55 +63,87 @@ function MapHelper() {
       >
         <Box
           sx={{
-            maxHeight: device === "mobile" ? "30vw" : "20vw",
-            minHeight: device === "mobile" ? "30vw" : "20vw",
-            maxWidth: device === "mobile" ? "60vw" : "20vw",
-            minWidth: device === "mobile" ? "60vw" : "20vw",
+            maxHeight: device === "mobile" ? "40vh" : "50vh",
+            minHeight: device === "mobile" ? "40vh" : "50vh",
+            maxWidth: device === "mobile" ? "60vw" : "30vw",
+            minWidth: device === "mobile" ? "60vw" : "30vw",
             overflow: "auto",
+            padding: "20px 20px",
           }}
         >
-          Karşınızdaki haritada bölgelere çıkan yuvarlakların üstüne tıkayarak
-          yaklaşın. Bölgelere yaklaştıkça bilgiler haritada yer yer
-          görünecektir. Yakınlaşmayı sürdürdükçe bilgilere erişeceksiniz. Mavi
-          pinlere tıklayıp bilgileri görüntüleyin.
+          <Chip
+            sx={{ backgroundColor: "#2596be", color: "white" }}
+            label={"Nasıl Kullanırım"}
+          />
+          <br />
+          <ul>
+            <li>
+              Karşınızdaki haritada bölgelere çıkan yuvarlakların üstüne
+              tıkayarak yaklaşın.
+            </li>
+            <li>
+              Bölgelere yaklaştıkça bilgiler haritada yer yer görünecektir.
+            </li>
+            <li>
+              Yakınlaşmayı sürdürdükçe bilgilere erişeceksiniz. Mavi pinlere
+              tıklayıp bilgileri görüntüleyin.
+            </li>
+            <li>
+              En başta yeradı/adres bilgisini okuyacaksınız. Düğmeler ile
+              amacına uygun ilerlemek isteyebilirsiniz.
+            </li>
+          </ul>
+          <Chip
+            sx={{ backgroundColor: "#2596be", color: "white" }}
+            label={"Kullanma Yöntemleri"}
+          />
+          <ul>
+            <li>*+*: artı düğmesi ile haritayı yakınlaştırıp bakın.</li>
+            <li>*-*: eksi düğmesi ile haritayı uzaklaştırıp bakın.</li>
+          </ul>
+          <Chip
+            sx={{ backgroundColor: "#2596be", color: "white" }}
+            label={"Renkli Yuvarlakların Anlamları"}
+          />
           <br />
           <br />
-          En başta yeradı/adres bilgisini okuyacaksınız. Düğmeler ile amacına
-          uygun ilerlemek isteyebilirsiniz.
-          <br />
-          <h4>Kullanma Yöntemleri</h4>
-          <br />
-          1- *+*: artı düğmesi ile haritayı yakınlaştırıp bakın.
-          <br />
-          2- *-*: eksi düğmesi ile haritayı uzaklaştırıp bakın.
-          <br />
-          ## Renkli Yuvarlakların Anlamları
-          <br />
-          <b>**YEŞİL**:</b>
-          <br />
-          <b>**ALAYEŞİL**: Düşük yoğunluk</b>
-          <br />
-          <b>**SARI**: Orta-alt yoğunluk</b>
-          <br />
-          <b>**TURUNCU**: Orta yoğunluk</b>
-          <br />
-          <b>**ALAKIZIL**: Orta-üst yoğunluk</b>
-          <br />
-          <b>**KIZIL**: Yüksek yoğunluk</b>
-          <br />
+          <div className={" map_helper"}>
+            {Object.keys(Tags).map((intensity) => (
+              <Button
+                key={intensity}
+                className="map_helper_button"
+                sx={{
+                  color: "black",
+                  boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+                  margin: "5px 10px",
+                }}
+                aria-label={Tags[intensity].intensity}
+                onClick={(event) => handleClick(event, intensity)}
+              >
+                <div
+                  className={styles.legend_item__color}
+                  style={{ backgroundColor: Tags[intensity].color }}
+                />
+                <span>&nbsp;&nbsp;{Tags[intensity].intensity}</span>
+              </Button>
+            ))}
+          </div>
           <br />
           <br />
-          <h3> UYARI </h3>
+          <Chip
+            sx={{ backgroundColor: "#2596be", color: "white" }}
+            label={"UYARI"}
+          />
           <br />
-          Yardım İçeriği bölümünde bir bilgi, bir twit karşınıza çıkmıyor ise
-          şunları deneyiniz.
-          <br />
-          1- Kaynak düğmesine tıklayıp bilgiye ulaşabilirsiniz.
-          <br />
-          2- Kayıtlı veriyi göster düğmesini etkin kılınız.
-          <br />
-          <br />
-          Bunların dışında bilgi bir biçimde erişilemez olmuştur.
+          <ul>
+            <li>
+              Yardım İçeriği bölümünde bir bilgi, bir twit karşınıza çıkmıyor
+              ise şunları deneyiniz.
+            </li>
+            <li>Kaynak düğmesine tıklayıp bilgiye ulaşabilirsiniz.</li>
+            <li>Kayıtlı veriyi göster düğmesini etkin kılınız.</li>
+            <li>Bunların dışında bilgi bir biçimde erişilemez olmuştur.</li>
+          </ul>
         </Box>
       </Popover>
     </>
