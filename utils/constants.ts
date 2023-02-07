cc06bd65c6848a9825489ab370779a8489693a5f
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-02-08 01:15:39 +0300

    fix: new BASEURL

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  "https://api.afetharita.com/tweets/areas?ne_lat=100&ne_lng=0&sw_lat=100&sw_lng=0";
+  "https://api.afetharita.com/feeds/areas?ne_lat=100&ne_lng=0&sw_lat=100&sw_lng=0";
