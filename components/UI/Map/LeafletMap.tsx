db686d1952b31b7118d8b6d96db5706ea6c0022f
Eray Gündoğmuş <74984741+eraygundogmus@users.noreply.github.com>
2023-02-07 09:11:08 +0300

    Revert "feat: add impacted city button to the right side"

--- components/UI/Map/LeafletMap.tsx
+++ components/UI/Map/LeafletMap.tsx
@@ -19,10 +19,6 @@ const MapLegend = dynamic(() => import("./MapLegend"), {
   ssr: false,
 });
 
-const ImpactedCities = dynamic(() => import("./ImpactedCities"), {
-  ssr: false,
-});
-
 type Props = {
   data: MarkerData[];
   onClickMarker: (_e: LeafletMouseEvent, _markerData: MarkerData) => void;
@@ -42,7 +38,6 @@ function LeafletMap({ onClickMarker, data, onClusterClick }: Props) {
     <>
       <MapLegend />
       <Map center={DEFAULT_CENTER} zoom={DEFAULT_ZOOM}>
-        <ImpactedCities />
         <TileLayer
           url={`https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&apistyle=s.e%3Al.i%7Cp.v%3Aoff%2Cs.t%3A3%7Cs.e%3Ag%7C`}
         />
