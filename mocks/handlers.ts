2717911d329d97718221b376d18d0d0b2c13c22f
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-02-08 00:58:36 +0300

    fix: wip

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -6,7 +6,7 @@ import areasJson from "./areasResponse.json";
 export const handlers = [
   // URL değişecek
   rest.get(
-    "https://api.afetharita.com/feeds/areas?ne_lat=100&ne_lng=0&sw_lat=100&sw_lng=0",
+    "https://api.afetharita.com/tweets/areas?ne_lat=100&ne_lng=0&sw_lat=100&sw_lng=0",
     (_req, res, ctx) => {
       return res(ctx.json<Data>(areasJson as Data));
     }
