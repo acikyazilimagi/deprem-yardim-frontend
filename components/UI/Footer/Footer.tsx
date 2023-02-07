0dc45f4b73b029e286aab021cdd56abc23448ca6
Eytan Anjel <eytanfb@gmail.com>
2023-02-07 15:13:44 -0800

    sozlesmeler yeni tabde aciliyor

--- components/UI/Footer/Footer.tsx
+++ components/UI/Footer/Footer.tsx
@@ -4,12 +4,12 @@ export default function Footer() {
   return (
     <ul className={styles.footer}>
       <li>
-        <a href="./cerez.docx" download>
+        <a href="./cerez.pdf" target="_blank">
           Çerez Politikası
         </a>
       </li>
       <li>
-        <a href="./gizlilik.docx" download>
+        <a href="./gizlilik.pdf" target="_blank">
           Gizlilik Politikası
         </a>
       </li>
