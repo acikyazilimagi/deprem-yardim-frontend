04bdfef5ceb78427e846286e6e9c9284c0507213
Emre Ceyhan <junk@emreceyhan.com>
2023-02-08 17:39:39 +0300

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://apigo.afetharita.com/feeds/areas";
+export const BASE_URL = "https://apigo.afetharita.com/feeds/areas";
 export const EXPAND_COORDINATE_BY_VALUE = 0.001;
