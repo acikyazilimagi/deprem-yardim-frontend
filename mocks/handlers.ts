affbaea0aef24139a63a25d2d1829b2fa2a3d1c8
Caner Dagli <cdagli@gmail.com>
2023-02-09 01:11:22 +0100

    feat: cleanup handlers

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -2,11 +2,7 @@ import { rest } from "msw";
 import { DataLite } from "./TypesAreasEndpoint";
 
 import areasJson from "./areasResponse.json";
-import { BASE_URL } from "@/utils/constants";
 import { areasURL } from "@/utils/urls";
-
-//"https://api.afetharita.com/tweets/areas?ne_lat=100&ne_lng=0&sw_lat=100&sw_lng=0",
-const url = BASE_URL;
 
 export const handlers = [
 
