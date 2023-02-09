826aba270c45a7eea84057ecef421e20e8f07539
ilker <ilker897@gmail.com>
2023-02-09 14:51:18 +0300

    style: website popups position and refactor

--- components/UI/SitesIcon/Icons.tsx
+++ components/UI/SitesIcon/Icons.tsx
@@ -8,6 +8,30 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 
 import React from "react";
 
+const webSiteLinks = [
+  {
+    name: "depremYardim",
+    link: "https://depremyardim.com/",
+    avatar: "/icons/depremyardımIcon.svg",
+    alt: "deprem yardim icon",
+    text: "Depremzedeleri aramak ve yardım etmek için kullandığımız site.",
+  },
+  {
+    name: "depremIO",
+    link: "https://deprem.io/",
+    avatar: "/icons/depremIOIcon.svg",
+    alt: "deprem io icon",
+    text: "Depremzedelere erzak yardımı için kullandığımız site.",
+  },
+  {
+    name: "depremDiscord",
+    link: "https://discord.gg/itdepremyardim",
+    avatar: "/icons/discordIcon.svg",
+    alt: "discord icon",
+    text: "Projelerimizi geliştirmek ve birbirimizle iletişim kurmak için kullandığımız Discord sunucusu.",
+  },
+];
+
 const SitesIcon = () => {
   const isMinWidth = useMediaQuery("(min-width:1024px)");
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -56,126 +80,45 @@ const SitesIcon = () => {
           zIndex: 500,
         }}
       >
-        <Box sx={toBiggerIcon}>
-          <Link
-            href="https://depremyardim.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            onMouseEnter={() => handlePopoverOpen("depremYardim")}
-            onMouseLeave={handlePopoverClose}
-          >
-            <Avatar
-              sx={littleIcon}
-              alt="deprem yardim icon"
-              src="/icons/depremyardımIcon.svg"
-            />
-          </Link>
-
-          <Popover
-            anchorReference="anchorEl"
-            anchorEl={anchorEl}
-            open={"depremYardim" === isOpen ? true : false}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            sx={{
-              background: "#00000000",
-              pointerEvents: "none",
-              zIndex: 600,
-            }}
-            onClose={handlePopoverClose}
-          >
-            <Typography sx={{ padding: "10px", width: "450px" }}>
-              Depremzedeleri aramak ve yardım etmek için kullandığımız site.
-            </Typography>
-          </Popover>
-        </Box>
-
-        <Box sx={toBiggerIcon}>
-          <Link
-            href="https://deprem.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-            onMouseEnter={() => handlePopoverOpen("depremIO")}
-            onMouseLeave={handlePopoverClose}
-          >
-            <Avatar
-              sx={littleIcon}
-              alt="deprem io icon"
-              src="/icons/depremIOIcon.svg"
-            />
-          </Link>
-
-          <Popover
-            anchorReference="anchorEl"
-            anchorEl={anchorEl}
-            open={"depremIO" === isOpen ? true : false}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            sx={{
-              background: "#00000000",
-              pointerEvents: "none",
-              zIndex: 600,
-            }}
-            onClose={handlePopoverClose}
-          >
-            <Typography sx={{ padding: "10px", width: "450px" }}>
-              Depremzedelere erzak yardımı için kullandığımız site.
-            </Typography>
-          </Popover>
-        </Box>
-
-        <Box sx={toBiggerIcon}>
-          <Link
-            href="https://discord.gg/itdepremyardim"
-            target="_blank"
-            rel="noopener noreferrer"
-            onMouseEnter={() => handlePopoverOpen("depremDiscord")}
-            onMouseLeave={handlePopoverClose}
-          >
-            <Avatar
-              sx={littleIcon}
-              alt="discord icon"
-              src="/icons/discordIcon.svg"
-            />
-          </Link>
-
-          <Popover
-            anchorReference="anchorEl"
-            anchorEl={anchorEl}
-            open={"depremDiscord" === isOpen ? true : false}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            sx={{
-              background: "#00000000",
-              pointerEvents: "none",
-              zIndex: 600,
-            }}
-            onClose={handlePopoverClose}
-          >
-            <Typography sx={{ padding: "10px", width: "450px" }}>
-              Projelerimizi geliştirmek ve birbirimizle iletişim kurmak için
-              kullandığımız Discord sunucusu.
-            </Typography>
-          </Popover>
-        </Box>
+        {webSiteLinks.map((item) => (
+          <Box key={item.name} sx={toBiggerIcon}>
+            <Link
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              onMouseEnter={() => handlePopoverOpen(item.name)}
+              onMouseLeave={handlePopoverClose}
+            >
+              <Avatar sx={littleIcon} alt={item.alt} src={item.avatar} />
+            </Link>
+            <Popover
+              anchorReference="anchorEl"
+              anchorEl={anchorEl}
+              open={item.name === isOpen ? true : false}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              sx={{
+                background: "#00000000",
+                pointerEvents: "none",
+                zIndex: 600,
+              }}
+              onClose={handlePopoverClose}
+            >
+              <Typography
+                fontSize={14}
+                sx={{ padding: "10px", width: "450px" }}
+              >
+                {item.text}
+              </Typography>
+            </Popover>
+          </Box>
+        ))}
       </Stack>
     </>
   );
