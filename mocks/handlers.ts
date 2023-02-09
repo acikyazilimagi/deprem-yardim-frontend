f6fe9f702ecc319f27895bf4890744ca35fd5730
Caner Dagli <cdagli@gmail.com>
2023-02-09 01:10:38 +0100

    feat: fix mocks

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,8 +1,9 @@
 import { rest } from "msw";
-import { Data } from "./TypesAreasEndpoint";
+import { DataLite } from "./TypesAreasEndpoint";
 
 import areasJson from "./areasResponse.json";
 import { BASE_URL } from "@/utils/constants";
+import { areasURL } from "@/utils/urls";
 
 //"https://api.afetharita.com/tweets/areas?ne_lat=100&ne_lng=0&sw_lat=100&sw_lng=0",
 const url = BASE_URL;
@@ -10,9 +11,9 @@ const url = BASE_URL;
 export const handlers = [
 
   rest.get(
-    url,
+    `${areasURL}/*`,
     (_req, res, ctx) => {
-      return res(ctx.json<Data>(areasJson as Data));
+      return res(ctx.json<DataLite>(areasJson as DataLite));
     }
   ),
 ];
