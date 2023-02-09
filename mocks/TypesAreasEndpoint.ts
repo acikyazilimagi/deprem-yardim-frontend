565e94b12b40eaebb42109e636608330795d9758
Caner Dagli <cdagli@gmail.com>
2023-02-09 02:41:49 +0100

    feat: adjust types

--- mocks/TypesAreasEndpoint.ts
+++ mocks/TypesAreasEndpoint.ts
@@ -1,25 +1,11 @@
-import { Point } from "./types";
-
 export type Data = {
     id: number;
     full_text: string;
     formatted_address: string;
-    loc: [number, number];
-    viewport: {
-      northeast: Point;
-      southwest: Point;
-    };
-    raw: Raw;
-    resolution: {
-      address?: string | null;
-      city?: string | null;
-      distinct?: string | null;
-      neighbourhood?: string | null;
-      street?: string | null;
-      no?: string | null;
-      name_surname?: string | null;
-      tel?: string | null;
-    };
+    extra_parameters?: string,
+    timestamp: string;
+    is_resolved: boolean;
+    channel: string;
 };
 
 export type Raw = {
