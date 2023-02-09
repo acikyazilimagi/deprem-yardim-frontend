0547355020542fbd54fb91bceced0612e7159c62
Caner Dagli <cdagli@gmail.com>
2023-02-09 02:20:12 +0100

    feat: update endpoint

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,5 +1,5 @@
 import { BASE_URL } from "./constants";
 
-export const areasURL = `${BASE_URL}/feeds/areas-lite`;
+export const areasURL = `${BASE_URL}/feeds/areas`;
 export const locationsURL = (locationId?: number) =>
   locationId ? `${BASE_URL}/feeds/${locationId}` : null;
