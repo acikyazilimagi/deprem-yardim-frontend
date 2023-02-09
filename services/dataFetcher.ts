543b2293f63d5aee4da5885f20a3bc57fa84b245
Caner Dagli <cdagli@gmail.com>
2023-02-09 01:43:50 +0100

    feat: remove commented

--- services/dataFetcher.ts
+++ services/dataFetcher.ts
@@ -2,14 +2,3 @@ export const dataFetcher = async (url: string) => {
   const response = await fetch(url);
   return response.json();
 };
-// fetch(url)
-//   .then((res) => res.json())
-//   .then((res) => {
-//     const transformedData = res.results ? dataTransformer(res) : [];
-//     setMarkerData(transformedData);
-//     return transformedData;
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//     throw error;
-//   });
