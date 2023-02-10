945aa55dc36c834d5ed42ad1d1447fc3b32177e0
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-02-11 00:09:00 +0300

    refactor: babala extra parameters refactor

--- components/UI/Drawer/components/channels/babala/FeedChannelBabala.tsx
+++ components/UI/Drawer/components/channels/babala/FeedChannelBabala.tsx
@@ -1,121 +1,73 @@
 import Typography from "@mui/material/Typography";
-import { Check } from "@mui/icons-material";
+import Chip from "@mui/material/Chip";
 
 import { FeedChannelBabalaProps } from "../../types";
-import { Link, Popover } from "@mui/material";
-import { useState } from "react";
+import { Divider } from "@mui/material";
 
 const FeedChannelBabala = ({
   full_text,
-  extra_parameters,
   reason,
+  extra_parameters,
 }: FeedChannelBabalaProps) => {
-  const [isPopoverOpen, setIsPopoverOpen] = useState<any>(false);
+  const extraValues =
+    extra_parameters &&
+    Object.entries(extra_parameters).map(([k, v]) => {
+      if (v && v !== "nan") {
+        return (
+          <Typography key={k} style={styles.fullText}>
+            {v}
+          </Typography>
+        );
+      }
+    });
 
   return (
     <>
       <div style={styles.container}>
-        <div style={styles.user}></div>
         <div style={styles.logo_container}>
           <Typography style={styles.logo}>Babala</Typography>
-          {!!extra_parameters?.status && (
-            <Link
-              onMouseEnter={() => setIsPopoverOpen(true)}
-              onMouseLeave={() => setIsPopoverOpen(false)}
-            >
-              <Check style={styles.confirmation} />
-            </Link>
-          )}
-          {!!reason && (
-            <Typography style={styles.reason}>
-              {reason.toLowerCase()}
-            </Typography>
-          )}
+          <div style={styles.chip_container}>
+            {!!reason && <Chip label={reason.toLowerCase()} color="info" />}
+          </div>
         </div>
+        <Divider />
         <Typography style={styles.fullText}>{full_text}</Typography>
+        {extraValues}
       </div>
-      <Popover
-        anchorReference="anchorEl"
-        open={isPopoverOpen}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        sx={{
-          background: "#00000000",
-          pointerEvents: "none",
-          zIndex: 10000000,
-        }}
-        onClose={() => setIsPopoverOpen(false)}
-      >
-        <Typography sx={{ padding: "10px", width: "450px" }}>
-          Veri doğrulanmış.
-        </Typography>
-      </Popover>
     </>
   );
 };
 
-// TODO#642: Babala channelından gelen veriler için gösterilecek tasarım
 const styles = {
-  reason: {
-    backgroundColor: "#F05454",
-    fontWeight: 800,
-    color: "white",
-    fontFamily:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-    fontSize: 12,
-    width: "fit-content",
-    borderRadius: 6,
-    paddingRight: 10,
-    paddingLeft: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginLeft: "auto",
-    marginRight: 0,
+  chip_container: {
+    display: "flex",
+    gap: 5,
+    fontWeight: 500,
   },
   logo_container: {
     display: "flex",
     flexDirection: "row" as "row",
     alignItems: "center",
-    lineHeight: 0,
+    justifyContent: "space-between",
     marginBottom: 10,
   },
-  confirmation: {
-    backgroundColor: "green",
-    fontSize: 15,
-    fontWeight: 800,
-    color: "white",
-    marginLeft: 10,
-    borderRadius: 12,
-  },
   logo: {
-    color: "white",
     fontFamily:
       '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
     fontWeight: 800,
   },
   container: {
     padding: "11px 15px 40px 15px",
-    borderRadius: "12px",
     margin: "10px 0",
-    backgroundColor: "#222831",
-  },
-  user: {
-    display: "flex",
-    justifyContent: "space-between",
-    paddingBottom: 11,
+    borderRadius: "12px",
+    backgroundColor: "#fff",
+    border: "1px solid #e0e0e0",
   },
   fullText: {
     fontFamily:
       '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
     fontSize: 14,
     fontWeight: 500,
-    color: "white",
     lineHeight: 1.35,
     marginTop: 20,
   },
