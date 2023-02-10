c90e5bfc9d39fa8025ed2c99bf82b8beb97b6095
eraygundogmus <eraygundogmus@teknasyon.com>
2023-02-10 18:43:19 +0300

    fix: error page texts

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,8 +4,9 @@ import type { NextPage } from "next";
 const NotFoundPage: NextPage = () => {
   return (
     <CustomErrorPage
-      title="Sayfa Bulunamadı"
-      detail="Aradığınız sayfa bulunamadı."
+      title="Lütfen aralıklı olarak tekrar dene. Hataları düzeltmek için sürekli çalışıyoruz"
+      detail="Umudunu kaybetme, birazdan tekrar dene. Hata aldığın için üzgünüz. Hataları düzeltmek için sürekli çalışıyoruz.
+      Lütfen aralıklı olarak tekrar dene. Bu websitesi gönüllü yüzlerce yazılımcı tarafından sürekli test edilip, geliştirilmeye devam ediyor. Vazgeçmeyeceğiz. Yanınızdayız. "
       statusCode={404}
     />
   );
