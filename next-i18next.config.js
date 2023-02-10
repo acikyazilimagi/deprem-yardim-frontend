b80f1e4e00335c4163d0b17b333ea3579f353e4c
Caner Dagli <cdagli@gmail.com>
2023-02-11 00:17:35 +0100

    fix: path resolve

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   i18n: {
     defaultLocale: "tr",
     locales: ["en", "tr"],
-    localePath: path.resolve("./public/static/locales"),
+    localePath: path.resolve("./public/locales"),
   },
 };
