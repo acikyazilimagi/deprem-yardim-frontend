38e51551f45dbcd81eb015f98b7c94d750d851e2
Caner Dagli <cdagli@gmail.com>
2023-02-10 15:38:25 +0100

    fix: use 17 instead

--- components/UI/Map/ClusterGroup.tsx
+++ components/UI/Map/ClusterGroup.tsx
@@ -1,5 +1,5 @@
 import { useMapClickHandlers } from "@/hooks/useMapClickHandlers";
-import { Fragment, MouseEvent } from "react";
+import { MouseEvent } from "react";
 import { findTagByClusterCount } from "../Tag/Tag.types";
 import L from "leaflet";
 import { MarkerData } from "@/mocks/types";
@@ -54,7 +54,7 @@ const ClusterGroup = ({ data }: Props) => {
       bounds.getNorthEast().lat,
     ],
     zoom: map.getZoom(),
-    options: { radius: 150 },
+    options: { radius: 150, maxZoom: 17 },
   });
 
   return (
