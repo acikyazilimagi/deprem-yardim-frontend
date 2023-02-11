90f15da94a554133ad849b2bd9ee2e76cd635e2b
RedCore <42092152+emirhnergn@users.noreply.github.com>
2023-02-11 16:13:05 +0300

    Revert "feat: add helpful links to mobile footer"
    This reverts commit 5684000506488eeb36fe129d618f171760f68722.

--- components/UI/Footer/Footer.tsx
+++ components/UI/Footer/Footer.tsx
@@ -1,10 +1,8 @@
 import styles from "./Footer.module.css";
 import { useTranslation } from "next-i18next";
-import { useDevice } from "@/stores/mapStore";
 
 export default function Footer() {
   const { t } = useTranslation("common");
-  const isMobile = useDevice() === "mobile";
   return (
     <ul className={styles.footer}>
       <li>
@@ -17,21 +15,6 @@ export default function Footer() {
           {t("footer.politic.privacy")}
         </a>
       </li>
-
-      {isMobile && (
-        <>
-          <li>
-            <a href="https://deprem.io/" target="_blank" rel="noreferrer">
-              Deprem.io
-            </a>
-          </li>
-          <li>
-            <a href="https://beniyiyim.com/" target="_blank" rel="noreferrer">
-              beniyiyim
-            </a>
-          </li>
-        </>
-      )}
     </ul>
   );
 }
