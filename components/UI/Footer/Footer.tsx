5684000506488eeb36fe129d618f171760f68722
RedCore <42092152+emirhnergn@users.noreply.github.com>
2023-02-11 12:47:07 +0300

    feat: add helpful links to mobile footer

--- components/UI/Footer/Footer.tsx
+++ components/UI/Footer/Footer.tsx
@@ -1,8 +1,10 @@
 import styles from "./Footer.module.css";
 import { useTranslation } from "next-i18next";
+import { useDevice } from "@/stores/mapStore";
 
 export default function Footer() {
   const { t } = useTranslation("common");
+  const isMobile = useDevice() === "mobile";
   return (
     <ul className={styles.footer}>
       <li>
@@ -15,6 +17,21 @@ export default function Footer() {
           {t("footer.politic.privacy")}
         </a>
       </li>
+
+      {isMobile && (
+        <>
+          <li>
+            <a href="https://deprem.io/" target="_blank" rel="noreferrer">
+              Deprem.io
+            </a>
+          </li>
+          <li>
+            <a href="https://beniyiyim.com/" target="_blank" rel="noreferrer">
+              beniyiyim
+            </a>
+          </li>
+        </>
+      )}
     </ul>
   );
 }
