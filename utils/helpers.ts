177b9f7ab441b91c8485512caf36b5be823985fe
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2023-02-13 05:39:37 +0300

    fix: Add `dirty-json` package to prevent JSON parsing errors

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,6 +1,5 @@
-const isNaN = (value: any) => {
-  return !!value && value.toLowerCase() === "nan";
-};
+const isNaN = (value: unknown) =>
+  typeof value === "string" && value.toLowerCase() === "nan";
 
 const capitalize = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
