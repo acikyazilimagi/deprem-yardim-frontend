8264c43e00ccf27eb230db87d113a1ded3a2873a
fdemir <frndemir@gmail.com>
2023-02-13 23:51:23 +0300

    chore: remove max warnings
    there are so many warnings that we will remove later

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
-const buildEslintCommand = (filenames) =>
-  `npx next lint --fix --max-warnings=0`;
+const buildEslintCommand = (filenames) => `npx next lint --fix`;
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
