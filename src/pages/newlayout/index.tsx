9a89b94775f9f92a708068548a0d119bc1b5326d
Göksenin Güngör <goksenin.gungor@wunder.is>
2023-02-15 01:38:13 +0300

    chore: theme adjustments

--- src/pages/newlayout/index.tsx
+++ src/pages/newlayout/index.tsx
@@ -7,17 +7,82 @@ import {
 } from "@/components/UI/Map/utils";
 
 import dynamic from "next/dynamic";
+import { Box, FormControlLabel, Switch } from "@mui/material";
+import {
+  useHelpView,
+  HelpViewComponent,
+} from "../../newlayout/components/HelpViewComponent/HelpViewComponent";
 
 const TileLayer = dynamic(
   () => import("react-leaflet").then((mod) => mod.TileLayer),
-  {
-    ssr: false,
-  }
+  { ssr: false }
 );
 
 const Map = dynamic(() => import("@/components/UI/Map/Map"), {
   ssr: false,
 });
+
+// Development toggle menu for overlays
+const DevelopmentToggleMenu = () => {
+  const helpView = useHelpView();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        zIndex: 90000,
+      }}
+    >
+      <FormControlLabel
+        control={
+          <Switch checked={helpView.isOpen} onChange={helpView.toggle} />
+        }
+        label="Show HelpView"
+      />
+      <FormControlLabel
+        control={
+          <Switch checked={helpView.isOpen} onChange={helpView.toggle} />
+        }
+        label="Show HelpView"
+      />
+      <FormControlLabel
+        control={
+          <Switch checked={helpView.isOpen} onChange={helpView.toggle} />
+        }
+        label="Show HelpView"
+      />
+      <FormControlLabel
+        control={
+          <Switch checked={helpView.isOpen} onChange={helpView.toggle} />
+        }
+        label="Show HelpView"
+      />
+    </Box>
+  );
+};
+
+// Development overlay container
+const UIElementsOverlay = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 90000,
+        padding: "1em",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <HelpViewComponent />
+    </Box>
+  );
+};
 
 const NHome = () => {
   const { defaultZoom } = useDefaultZoom();
@@ -25,29 +90,33 @@ const NHome = () => {
   const device = useDevice();
   return (
     <>
-      <Map
-        center={defaultCenter}
-        zoom={defaultZoom}
-        minZoom={
-          device === "desktop"
-            ? DEFAULT_MIN_ZOOM_DESKTOP
-            : DEFAULT_MIN_ZOOM_MOBILE
-        }
-        zoomSnap={1}
-        zoomDelta={1}
-        preferCanvas
-        maxBoundsViscosity={1}
-        maxZoom={18}
-        whenReady={(map: any) => {
-          setTimeout(() => {
-            map.target.invalidateSize();
-          }, 100);
-        }}
-      >
-        <TileLayer
-          url={`https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&apistyle=s.e%3Al.i%7Cp.v%3Aoff%2Cs.t%3A3%7Cs.e%3Ag%7C`}
-        />
-      </Map>
+      <UIElementsOverlay />
+      <DevelopmentToggleMenu />
+      <>
+        <Map
+          center={defaultCenter}
+          zoom={defaultZoom}
+          minZoom={
+            device === "desktop"
+              ? DEFAULT_MIN_ZOOM_DESKTOP
+              : DEFAULT_MIN_ZOOM_MOBILE
+          }
+          zoomSnap={1}
+          zoomDelta={1}
+          preferCanvas
+          maxBoundsViscosity={1}
+          maxZoom={18}
+          whenReady={(map: any) => {
+            setTimeout(() => {
+              map.target.invalidateSize();
+            }, 100);
+          }}
+        >
+          <TileLayer
+            url={`https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&apistyle=s.e%3Al.i%7Cp.v%3Aoff%2Cs.t%3A3%7Cs.e%3Ag%7C`}
+          />
+        </Map>
+      </>
     </>
   );
 };
