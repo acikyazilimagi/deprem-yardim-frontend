92fda8e11ab294d94fb5986cea69dcdb37a9b693
Göksenin Güngör <goksenin.gungor@wunder.is>
2023-02-14 23:37:18 +0300

    chore: init newlayout page

---
+++ src/pages/newlayout/index.tsx
@@ -0,0 +1,43 @@
+import Map from "@/components/UI/Map/Map";
+import useDefaultZoom from "@/hooks/useDefaultZoom";
+import useDefaultCenter from "@/hooks/useDefaultCenter";
+import { useDevice } from "@/stores/mapStore";
+import {
+  DEFAULT_MIN_ZOOM_DESKTOP,
+  DEFAULT_MIN_ZOOM_MOBILE,
+} from "@/components/UI/Map/utils";
+import { TileLayer } from "react-leaflet";
+
+const NHome = () => {
+  const { defaultZoom } = useDefaultZoom();
+  const { defaultCenter } = useDefaultCenter();
+  const device = useDevice();
+  return (
+    <>
+      <Map
+        center={defaultCenter}
+        zoom={defaultZoom}
+        minZoom={
+          device === "desktop"
+            ? DEFAULT_MIN_ZOOM_DESKTOP
+            : DEFAULT_MIN_ZOOM_MOBILE
+        }
+        zoomSnap={1}
+        zoomDelta={1}
+        preferCanvas
+        maxBoundsViscosity={1}
+        maxZoom={18}
+        whenReady={(map: any) => {
+          setTimeout(() => {
+            map.target.invalidateSize();
+          }, 100);
+        }}
+      >
+        <TileLayer
+          url={`https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&apistyle=s.e%3Al.i%7Cp.v%3Aoff%2Cs.t%3A3%7Cs.e%3Ag%7C`}
+        />
+      </Map>
+    </>
+  );
+};
+export default NHome;
