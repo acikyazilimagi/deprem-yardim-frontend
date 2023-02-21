efb0c2d1f2976440131eb29ad34c9286dcc36cb0
Mert Kaya <mertkaya1033@gmail.com>
2023-02-21 00:21:21 -0500

    refactor: extract useful time utils from old ui

---
+++ src/utils/filterTime.ts
@@ -0,0 +1,70 @@
+export type TimeOption =
+  | "last30Minutes"
+  | "last1Hour"
+  | "last3Hours"
+  | "last6Hours"
+  | "last12Hours"
+  | "last24Hours"
+  | "last3Days"
+  | "all";
+
+export type FilterOption = {
+  label: string;
+  inMilliseconds: number;
+  value: TimeOption;
+};
+
+// TODO: move to constants file
+const HOUR_IN_MILLISECONDS = 60 * 60 * 1000;
+const DAY_IN_MILLISECONDS = 24 * HOUR_IN_MILLISECONDS;
+
+export const FilterOptions: readonly FilterOption[] = [
+  {
+    label: "lastHalfHour",
+    inMilliseconds: (1 * HOUR_IN_MILLISECONDS) / 2,
+    value: "last30Minutes",
+  },
+  {
+    label: "lastHour",
+    inMilliseconds: 1 * HOUR_IN_MILLISECONDS,
+    value: "last1Hour",
+  },
+  {
+    label: "lastThreeHours",
+    inMilliseconds: 3 * HOUR_IN_MILLISECONDS,
+    value: "last3Hours",
+  },
+  {
+    label: "lastSixHours",
+    inMilliseconds: 6 * HOUR_IN_MILLISECONDS,
+    value: "last6Hours",
+  },
+  {
+    label: "lastTwelveHours",
+    inMilliseconds: 12 * HOUR_IN_MILLISECONDS,
+    value: "last12Hours",
+  },
+  {
+    label: "lastDay",
+    inMilliseconds: 24 * HOUR_IN_MILLISECONDS,
+    value: "last24Hours",
+  },
+  {
+    label: "lastThreeDays",
+    inMilliseconds: 3 * DAY_IN_MILLISECONDS,
+    value: "last3Days",
+  },
+  { label: "all", inMilliseconds: -1, value: "all" },
+] as const;
+
+export const valueToOption = (value: string): FilterOption | undefined => {
+  return FilterOptions.find((option) => option.value === value);
+};
+
+export const computeTimestamp = (option: FilterOption): number => {
+  const currentTimestampInMillis = Date.now().valueOf();
+  const pastTimestampInMillis =
+    currentTimestampInMillis - option.inMilliseconds;
+  const pastTimestampInSeconds = Math.floor(pastTimestampInMillis / 1000);
+  return Math.floor(pastTimestampInSeconds);
+};
