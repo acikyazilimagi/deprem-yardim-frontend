a608625a6b41434a6d71245fd20803e0d88060e7
Mert Kaya <mertkaya1033@gmail.com>
2023-02-21 00:40:47 -0500

    fix: set default min zoom to an integer to fix loading issue

--- src/components/UI/Map/utils.ts
+++ src/components/UI/Map/utils.ts
@@ -8,8 +8,8 @@ export const DEFAULT_MAX_BOUNDS: LatLngBoundsExpression = [
   [39.684006, 31.607263],
   [35.459834, 45.281141],
 ];
-export const DEFAULT_MIN_ZOOM_DESKTOP = 6.4;
-export const DEFAULT_MIN_ZOOM_MOBILE = 4.5;
+export const DEFAULT_MIN_ZOOM_DESKTOP = 7;
+export const DEFAULT_MIN_ZOOM_MOBILE = 4;
 
 export const localStorageKeys = {
   coordinatesURL: "coordinatesURL",
