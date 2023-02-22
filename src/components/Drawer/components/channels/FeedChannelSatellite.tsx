4b6ed2229a8805d502ed1a738f4c60b2efea54f5
eraygundogmus <eraygundogmus@teknasyon.com>
2023-02-22 12:18:06 +0300

    refactor: clean up code

---
+++ src/components/Drawer/components/channels/FeedChannelSatellite.tsx
@@ -0,0 +1,26 @@
+import { DrawerContent } from "@/components/Drawer/components/channels/DrawerContent";
+import { capitalize } from "@/utils/helpers";
+
+interface FeedChannelSatelliteProps {
+  channel: string;
+  properties: {
+    damage: string;
+  };
+}
+
+export const FeedChannelSatellite = (props: FeedChannelSatelliteProps) => {
+  return (
+    <DrawerContent>
+      <DrawerContent.Header>
+        <DrawerContent.Title>Uydu Verisi</DrawerContent.Title>
+        <DrawerContent.Badge>
+          {capitalize(
+            props?.properties?.damage === "Destroyed"
+              ? "Collapsed"
+              : props?.properties?.damage
+          )}
+        </DrawerContent.Badge>
+      </DrawerContent.Header>
+    </DrawerContent>
+  );
+};
